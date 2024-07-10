c56c5f61cddfd74e64586587edb3608a8fa359af
asifthedev <90164370+asifthedev@users.noreply.github.com>
2024-07-10 15:35:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import os
 import streamlit as st
 from openai import OpenAI
 
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
 st.title("GPT Clone")
-client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 if "message" not in st.session_state:
     st.session_state["message"] = []
